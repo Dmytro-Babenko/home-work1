1cff8932862c00c7caea7454a1d76066317fecc1
Dmytro-Babenko <dmytro.babenko87@gmail.com>
2023-02-01 02:04:02 +0200

    Update sort.py

--- sort.py
+++ sort.py
@@ -5,53 +5,66 @@ def put_in_order(folder: pathlib.Path, category_by_extension: dict, unknown_exte
     for file in folder.iterdir():
         if file.name in files_categories:
             continue
+
         elif file.is_dir():
             try: 
                 file.rmdir()
-            except:
+            except OSError:
                 put_in_order(file, category_by_extension, unknown_extensions, known_extensions, files_categories)
+        
         else:
             extension = file.suffix
-            new_stem = normalize(TRANS, file.stem)
+            old_stem = file.stem
+            new_stem = normalize(TRANS, old_stem)
             new_name = f'{new_stem}{extension}'
             category = category_by_extension.get(extension)
             if category == None:
+                base_folder = folder
                 if extension not in unknown_extensions:
                     unknown_extensions.append(extension)
             else:
                 if extension not in known_extensions:
                     known_extensions.append(extension)
+                base_folder = folder.joinpath(category)
+                if not base_folder.exists():
+                    base_folder.mkdir()
 
-                if category == 'archives':
-                    files_categories[category].append(new_name)
+            if category == 'archives':
+                new_name, new_path = find_free_name(new_stem, base_folder)
+                shutil.unpack_archive(file, new_path)
+                file.unlink()
+            elif category != None or (category == None and new_stem != old_stem):
+                new_name, new_path = find_free_name(new_stem, base_folder, extension=extension)
+                file.rename(new_path)
 
-                else:
-                    category_folder = folder.joinpath(category)
-                    if not category_folder.exists():
-                        category_folder.mkdir()
+            if category != None:
+                files_categories[category].append(new_name)
                 
-                    try:
-                        file.rename(category_folder.joinpath(new_name))
-                    except FileExistsError:
-                        i = 1
-                        while True:
-                            try: 
-                                new_name = f'{new_stem}_{i}{extension}'
-                                file.rename(category_folder.joinpath(new_name))
-                            except FileExistsError:
-                                i += 1
-                            else:
-                                break
-
-                    files_categories[category].append(new_name)   
-    new_name = normalize(TRANS, folder.name)
-    folder.rename(folder.with_name(new_name)) 
+    old_folder_name = folder.name
+    new_folder_name = normalize(TRANS, old_folder_name)
+    if new_folder_name != old_folder_name:
+        new_folder_name, new_path = find_free_name(new_folder_name, folder.parent)
+        folder.rename(new_path) 
     return files_categories, unknown_extensions, known_extensions
 
 def normalize(TRANS: dict, old_name: str) -> str:
     name_translitarate = old_name.translate(TRANS)
     new_name = re.sub(r'\W', '_', name_translitarate) 
     return new_name
+
+def find_free_name (new_stem: str, base_folder: pathlib.Path, extension = ''):
+    new_name = f'{new_stem}{extension}'
+    new_path = base_folder.joinpath(new_name)
+    if new_path.exists():
+        i = 1
+        while True:
+            if not base_folder.joinpath(f'{new_stem}_{i}{extension}').exists():
+                new_name = f'{new_stem}_{i}{extension}'
+                new_path = base_folder.joinpath(new_name)
+                break
+            i += 1
+    return new_name, new_path
+
 
 def make_translitarate_table() -> dict:
     CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
@@ -64,13 +77,19 @@ def make_translitarate_table() -> dict:
     return TRANS
 
 TRANS = make_translitarate_table()
-path = r'D:\Мотлох'
+
 unknown_extensions = [] 
 known_extensions = []
 files_categories = {'archives' : [], 'video' : [], 'audio' : [], 'documents' : [], 'images' : []}
-category_by_extension = {'.jpeg': 'images', '.png': 'images', '.jpg': 'images', '.svg': 'images', '.avi': 'video', '.mp4': 'video', '.mov': 'video', '.mkv': 'video', '.doc': 'documents', '.docx': 'documents', '.txt': 'documents', '.pdf': 'documents', '.xlsx': 'documents', '.pptx': 'documents', '.mp3': 'music', '.ogg': 'music', '.wav': 'music', '.amr': 'music', '.zip': 'archives', '.gz': 'archives', '.tar': 'archives', '.rar': 'archives'}
+category_by_extension = {'.jpeg': 'images', '.png': 'images', '.jpg': 'images', '.svg': 'images', '.avi': 'video', '.mp4': 'video', '.mov': 'video', '.mkv': 'video', '.doc': 'documents', '.docx': 'documents', '.txt': 'documents', '.pdf': 'documents', '.xlsx': 'documents', '.pptx': 'documents', '.mp3': 'music', '.ogg': 'music', '.wav': 'music', '.amr': 'music', '.zip': 'archives', '.gz': 'archives', '.tar': 'archives'}
 
+path = r'D:\Мотлох'
+# path2 = r'D:\Мотлох\Новая папка(.zip'
 folder = pathlib.Path(path)
+# shutil.rmtree(folder)
+# folder.unlink()
+
+
 print(put_in_order(folder, category_by_extension, unknown_extensions, known_extensions, files_categories))
 
 
